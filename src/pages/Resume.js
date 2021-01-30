116803d89d94e38abfaff6c6f00fcf285276457d
Dan ONeil <dhoneil@gmail.com>
2021-01-30 17:02:43 -0500

    Added temporary pdf fix to Resume.js

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,16 +1,39 @@
 import React from "react";
 import Container from "../components/Container";
-import { Document, Page } from 'react-pdf';
-import resume from "../images/resume.pdf";
+// import { Document, Page } from 'react-pdf';
+import resume from "../images/resume.pdf"
+import resume1 from "../images/resume-pg-1.png";
+import resume2 from "../images/resume-pg-2.png";
 
 function About() {
   return (
     <div>
       <Container>
 
-        <Document file={resume}>
+        {/* <Document file={resume}>
           <Page pageNumber={1} />
-        </Document>
+        </Document> */}
+        <a
+          href={resume}
+          download
+          target="blank"
+        >
+          Download Resume
+        </a>
+        <br/>
+        <img
+          src={resume1}
+          alt={"Resume Page 1"} 
+          className="img-fluid"
+          style={{ maxWidth: "800px", width: "100%" }}
+        />
+        <img
+          src={resume2}
+          alt={"Resume Page 2"} 
+          className="img-fluid"
+          style={{ maxWidth: "800px", width: "100%" }}
+        />
+
 
       </Container>
     </div>
