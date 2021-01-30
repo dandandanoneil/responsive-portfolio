25e7a082c2e63a05f4c014580f17fd9338f6e4b2
Dan ONeil <dhoneil@gmail.com>
2021-01-30 16:05:30 -0500

    Switched from BrowserRouter to HashRouter

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Col from "../Col";
 
 function Project(props) {
   return (
     <Col size="md-6">
-      <Link to={props.project.gitHubLink} target="blank">
+      <a href={props.project.gitHubLink} target="blank">
         <img 
         src={props.project.image}
         alt={props.project.title} 
         className="img-fluid border border-primary"/>
-      </Link>
+      </a>
       <h5>{props.project.title}</h5>
       <div>
-        <Link to={props.project.gitHubLink} target="blank">
+        <a href={props.project.gitHubLink} target="blank">
           GitHub Repo
-        </Link>
+        </a>
         {" | "}
-        <Link to={props.project.deployedLink} target="blank">
+        <a href={props.project.deployedLink} target="blank">
           Deployed Web App
-        </Link>
+        </a>
         <div><i>{props.project.demoCredentials}</i></div>
         <div><i>{props.project.collaborators}</i></div>
         <h6>{props.project.technologies}</h6>
