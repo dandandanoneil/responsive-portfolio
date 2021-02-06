0bdad966609add5c72d127da6405f63dd4890e48
Dan ONeil <dhoneil@gmail.com>
2021-02-06 14:40:58 -0500

    Updated portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,10 +6,10 @@ import Project from "../components/Project";
 
 import vinylImage from "../images/portfolio/Vinyl Destination.png";
 import travelImage from "../images/portfolio/Travel Companion.png";
-import employeeImage from "../images/portfolio/Employee Directory.png";
+import bookImage from "../images/portfolio/Google Book Search.png";
 import passwordImage from "../images/portfolio/Password Generator.png";
+import employeeImage from "../images/portfolio/Employee Directory.png";
 import budgetImage from "../images/portfolio/Budget Tracker.png";
-import weatherImage from "../images/portfolio/Weather Dashboard.png";
 
 const projects = [
   {
@@ -33,14 +33,14 @@ const projects = [
     description: "A front-end web application that simulates the cultural experience of travel. Based on a user input (search, ask for a random city, or use my current location), the app makes external API calls to show tailored results from Goodreads, Open Movie Database, New York Times, and Harvard Art Museums."
   },
   {
-    title: "Employee Directory",
-    image: employeeImage,
-    gitHubLink: "https://dandandanoneil.github.io/employee-directory/",
-    deployedLink: "https://dandandanoneil.github.io/employee-directory/",
+    title: "Google Book Search",
+    image: bookImage,
+    gitHubLink: "https://dandandanoneil.github.io/google-book-search/",
+    deployedLink: "https://dandandanoneil-google-book-app.herokuapp.com/",
     demoCredentials: "",
     collaborators: "",
-    technologies: "JavaScript, React.js, Express, HTML5, Bootstrap Framework",
-    description: "A front-end React.js app using user data from randomuser.me that allows a user to easily view employee data, sort by name or age, ascending or descending, and can filter results using a name search feature."
+    technologies: "JavaScript, MongoDB, Express, React.js, Node.js, HTML5, Bootstrap Framework",
+    description: "A full stack web app that allows a user to search for books on the Google Books API and save data about book, cover art, and links to more information to their collection."
   },
   {
     title: "Password Generator",
@@ -53,6 +53,16 @@ const projects = [
     description: "A web application that generates secure, random passwords based on user inputs. User interface includes the ability to select what kinds of characters to include, easily copy the generated password to the clipboard, generate a new password with the same criteria, eliminate easily misread characters."
   },
   {
+    title: "Employee Directory",
+    image: employeeImage,
+    gitHubLink: "https://dandandanoneil.github.io/employee-directory/",
+    deployedLink: "https://dandandanoneil.github.io/employee-directory/",
+    demoCredentials: "",
+    collaborators: "",
+    technologies: "JavaScript, React.js, Express, HTML5, Bootstrap Framework",
+    description: "A front-end React.js app using user data from randomuser.me that allows a user to easily view employee data, sort by name or age, ascending or descending, and can filter results using a name search feature."
+  },
+  {
     title: "Budget Tracker",
     image: budgetImage,
     gitHubLink: "https://github.com/dandandanoneil/budget-tracker/",
@@ -61,16 +71,6 @@ const projects = [
     collaborators: "",
     technologies: "JavaScript, MongoDB, Express, HTML5, Bootstrap Framework",
     description: "A web application that utilizes MongoDB, Mongoose, Node, and Express to track a user's budget, and uses IndexedDB to log transactions while offline, then sync transactions when brought back online."
-  },
-  {
-    title: "Weather Dashboard",
-    image: weatherImage,
-    gitHubLink: "https://dandandanoneil.github.io/weather-dashboard/",
-    deployedLink: "https://dandandanoneil.github.io/weather-dashboard/",
-    demoCredentials: "",
-    collaborators: "",
-    technologies: "JavaScript, HTML5, Bootstrap Framework",
-    description: "A web application that uses the OpenWeather API to display weather forecast data for a city provided by the user - including temperature data, humidity, wind speed, and UV index."
   }
 ]
 
